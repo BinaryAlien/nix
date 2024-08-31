3bdaa12763784c9c8ddc5ef4fc26714987b97192
Binary <49949198+BinaryAlien@users.noreply.github.com>
2024-08-31 10:48:15 +0200

    home(neovim: telescope): swap 'find_files' and 'git_files' key mappings

--- home/neovim/plugin/telescope.nvim.lua
+++ home/neovim/plugin/telescope.nvim.lua
@@ -10,7 +10,7 @@ require("telescope").setup({
 })
 
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<Leader>E", builtin.find_files, {})
-vim.keymap.set("n", "<Leader>e", builtin.git_files, {})
+vim.keymap.set("n", "<Leader>E", builtin.git_files, {})
+vim.keymap.set("n", "<Leader>e", builtin.find_files, {})
 vim.keymap.set("n", "<Leader>g", builtin.live_grep, {})
 vim.keymap.set("n", "<Leader>h", builtin.help_tags, {})
