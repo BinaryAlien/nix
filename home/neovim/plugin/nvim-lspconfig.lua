544d23b242d0353c089bdb336d662b40ad101f47
Binary <49949198+BinaryAlien@users.noreply.github.com>
2024-11-24 17:56:25 +0100

    home(neovim: nvim-lspconfig): add 'vim.diagnostic.open_float' key mapping

--- home/neovim/plugin/nvim-lspconfig.lua
+++ home/neovim/plugin/nvim-lspconfig.lua
@@ -13,6 +13,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
     vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
     vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+    vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
     vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
     vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
     vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
