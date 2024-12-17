3418a1f9e5e882412183a9a10e31a9f5b04e782d
Binary <49949198+BinaryAlien@users.noreply.github.com>
2024-12-17 12:10:46 +0100

    home(neovim: nvim-lspconfig): override 'vim.diagnostic' jump key mappings

--- home/neovim/plugin/nvim-lspconfig.lua
+++ home/neovim/plugin/nvim-lspconfig.lua
@@ -9,6 +9,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
   desc = "LSP actions",
   callback = function(event)
     local opts = { buffer = event.buf }
+    vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
+    vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
     vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
     vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
     vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
