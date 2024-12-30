6faa4e8cb22ea1c7e5673354024f20a13893a38a
Binary <49949198+BinaryAlien@users.noreply.github.com>
2024-12-30 17:00:19 +0100

    home(neovim): export diagnostics from options

---
+++ home/neovim/diagnostics.lua
@@ -0,0 +1,15 @@
+-- Diagnostics
+vim.diagnostic.config({
+  severity_sort = true,
+
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.HINT] = "",
+      [vim.diagnostic.severity.INFO] = "",
+    },
+  },
+
+  virtual_text = false,
+})
--- home/neovim/options.lua
+++ home/neovim/options.lua
@@ -1,22 +1,12 @@
--- Diagnostics
-vim.diagnostic.config({
-  severity_sort = true,
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = "",
-      [vim.diagnostic.severity.WARN] = "",
-      [vim.diagnostic.severity.HINT] = "",
-      [vim.diagnostic.severity.INFO] = "",
-    },
-  },
-  virtual_text = false,
-})
-
 -- Editing
 vim.opt.backspace = ""
 vim.opt.expandtab = true
 vim.opt.mouse = ""
 vim.opt.shiftwidth = 4
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- Visual
 vim.opt.cursorline = true
@@ -25,7 +15,3 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.showmode = false
 vim.opt.signcolumn = "yes"
-
--- Search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
